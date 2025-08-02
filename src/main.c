e29661f85319aec342f407cd79d99c38e141e27a
Donovan Bale <donovan@balehaus.org>
2025-08-02 13:19:28 -0500

    moving triangle

--- src/main.c
+++ src/main.c
@@ -1,6 +1,15 @@
-#include <GL/glew.h>
+#include <glad/glad.h> 
 #include <GLFW/glfw3.h>
 #include <stdio.h>
+
+struct Triangle {
+  float vertex_a_x;
+  float vertex_a_y;
+  float vertex_b_x;
+  float vertex_b_y;
+  float vertex_c_x;
+  float vertex_c_y;
+};
 
 int main()
 {
@@ -11,7 +20,7 @@ int main()
   }
 
   // Create a window
-  GLFWwindow *window = glfwCreateWindow(800, 600, "Game Engine", NULL, NULL);
+  GLFWwindow *window = glfwCreateWindow(800, 600, "Familiars", NULL, NULL);
   if (!window)
   {
     glfwTerminate();
@@ -21,19 +30,60 @@ int main()
   // Make the window's context current
   glfwMakeContextCurrent(window);
 
+
+  if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
+  {
+    printf("Failed to initialize GLAD");
+    return -1;
+  }   
+
+  glViewport(0, 0, 800, 600);
+
+  struct Triangle t1;
+  t1.vertex_a_x = -0.5f;
+  t1.vertex_a_y = -0.5f;
+  t1.vertex_b_x = 0.5f;
+  t1.vertex_b_y = -0.5f;
+  t1.vertex_c_x = 0.0f;
+  t1.vertex_c_y = 0.5f;
+
   // Main game loop
   while (!glfwWindowShouldClose(window))
   { 
     // Input handling
     glfwPollEvents();
 
+    if (glfwGetKey(window, GLFW_KEY_LEFT) == GLFW_PRESS) {
+      t1.vertex_a_x -= 0.1f;
+      t1.vertex_b_x -= 0.1f;
+      t1.vertex_c_x -= 0.1f;
+    }
+
+    if (glfwGetKey(window, GLFW_KEY_RIGHT) == GLFW_PRESS) {
+      t1.vertex_a_x += 0.1f;
+      t1.vertex_b_x += 0.1f;
+      t1.vertex_c_x += 0.1f;
+    }
+
+    if (glfwGetKey(window, GLFW_KEY_UP) == GLFW_PRESS) {
+      t1.vertex_a_y += 0.1f;
+      t1.vertex_b_y += 0.1f;
+      t1.vertex_c_y += 0.1f;
+    }
+
+    if (glfwGetKey(window, GLFW_KEY_DOWN) == GLFW_PRESS) {
+      t1.vertex_a_y -= 0.1f;
+      t1.vertex_b_y -= 0.1f;
+      t1.vertex_c_y -= 0.1f;
+    }
+
     // Rendering code goes here
     glClear(GL_COLOR_BUFFER_BIT);
 
     glBegin(GL_TRIANGLES);
-    glVertex2f(-0.5f, -0.5f);
-    glVertex2f(0.5f, -0.5f);
-    glVertex2f(0.0f, 0.5f);
+    glVertex2f(t1.vertex_a_x, t1.vertex_a_y);
+    glVertex2f(t1.vertex_b_x, t1.vertex_b_y);
+    glVertex2f(t1.vertex_c_x, t1.vertex_c_y);
     glEnd();
 
     // Swap buffers
